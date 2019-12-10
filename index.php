1d83748ddc8dc231afbd6f7a3ced3aa80d412434
antoineegret <a.egret@laposte.net>
2019-12-10 15:58:11 +0100

    exo4 partie5 php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+//ceci et un tableau en php une genre de super variable
+//qui aurai des sous variable pour stocker plus d'information,
+$mois[1] = 'Janvier';
+$mois[2] = 'Février';
+$mois[3] = 'Mars';
+$mois[4] = 'Avril';
+$mois[5] = 'Mai';
+$mois[6] = 'Juin';
+$mois[7] = 'Juillet';
+$mois[8] = 'Août';
+$mois[9] = 'Septembre';
+$mois[10] = 'Octobre';
+$mois[11] = 'Novembre';
+$mois[12] = 'Décembre';
+//ici j'affiche avec echo la valeur de la 3eme case du tableau
+echo $mois[8];
+ ?>
